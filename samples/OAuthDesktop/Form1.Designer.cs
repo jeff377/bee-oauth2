979d423968fb6898e20337c4519ab70cc8eaf842
蔡明志 <jeff377@gmail.com>
2025-04-02 11:32:48 +0800

    ．新增 OAuthDesktop 專案，.NET 8 WinForm 的 OAuth2 測試專案。

---
+++ samples/OAuthDesktop/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace OAuthDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGoogle = new Button();
+            edtUserInfo = new TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGoogle
+            // 
+            btnGoogle.Location = new Point(12, 254);
+            btnGoogle.Name = "btnGoogle";
+            btnGoogle.Size = new Size(75, 23);
+            btnGoogle.TabIndex = 0;
+            btnGoogle.Text = "Google";
+            btnGoogle.UseVisualStyleBackColor = true;
+            btnGoogle.Click += this.btnGoogle_Click;
+            // 
+            // edtUserInfo
+            // 
+            edtUserInfo.Location = new Point(12, 12);
+            edtUserInfo.Multiline = true;
+            edtUserInfo.Name = "edtUserInfo";
+            edtUserInfo.Size = new Size(481, 236);
+            edtUserInfo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(505, 290);
+            this.Controls.Add(edtUserInfo);
+            this.Controls.Add(btnGoogle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += this.Form1_Load;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnGoogle;
+        private TextBox edtUserInfo;
+    }
+}
---
+++ samples/OAuthDesktop/Form1.cs
@@ -0,0 +1,70 @@
+using Bee.OAuth2;
+using Bee.OAuth2.Desktop;
+using Newtonsoft.Json;
+
+namespace OAuthDesktop
+{
+    public partial class Form1 : Form
+    {
+        private OAuthConfig? _config = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath =  "OAuthConfig.json";
+            _config = LoadOAuthConfig(filePath);
+        }
+
+        private OAuthConfig LoadOAuthConfig(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Configuration file not found.", filePath);
+
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<OAuthConfig>(json) ?? new OAuthConfig();
+        }
+
+        /// <summary>
+        /// 執行登入。
+        /// </summary>
+        /// <param name="options">OAuth2 設定選項。</param>
+        private async void Login(TOAuthOptions options)
+        {
+            var client = new TOAuthClient(options);
+            var result = await client.Login("OAuth2 登入", 800, 600);  // 開啟登入界面，用戶執行登入後，回傳用戶身份的 JSON 資料
+            ShowUserInfo(result.ProviderName, result.UserInfo);
+        }
+
+        /// <summary>
+        /// 顯示用戶資訊。
+        /// </summary>
+        private void ShowUserInfo(string providerName, TUserInfo userInfo)
+        {
+            if (userInfo == null)
+            {
+                edtUserInfo.Text = string.Empty;
+                return;
+            }
+            var json = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(userInfo.RawJson), Formatting.Indented);
+            var value = $"ProviderName : {providerName}\r\n" +
+                                $"UserID : {userInfo.UserId}\r\n" +
+                                $"UserName : {userInfo.UserName}\r\n" +
+                                $"Email : {userInfo.Email}\r\n" +
+                                $"RawJson : \r\n{json}";
+            edtUserInfo.Text = value;
+        }
+
+        private void btnGoogle_Click(object sender, EventArgs e)
+        {
+            var options = _config?.GoogleOAuth;
+            if (options == null) { return; }
+            this.Login(options);
+        }
+
+
+    }
+}
