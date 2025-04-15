49aab11a8561afc60ecd2d4b7c091eda32cc2f7e
蔡明志 <jeff377@gmail.com>
2025-04-15 23:01:11 +0800

    Code Review

---
+++ samples/OAuthAspNetCore/Extensions/OAuth2RegistrationHelper.cs
@@ -0,0 +1,42 @@
+﻿using Bee.OAuth2.AspNetCore;
+using OAuthAspNetCore.Models;
+
+namespace OAuthAspNetCore.Extensions
+{
+    public static class OAuth2RegistrationHelper
+    {
+        public static TOAuth2Manager CreateOAuth2Manager(IServiceProvider provider)
+        {
+            var config = provider.GetRequiredService<IConfiguration>();
+            var accessor = provider.GetRequiredService<IHttpContextAccessor>();
+
+            var oauthConfig = config.Get<OAuthConfig>();
+            var manager = new TOAuth2Manager(accessor);
+
+            if (oauthConfig == null)
+            {
+                throw new InvalidOperationException("OAuth configuration is missing.");
+            }
+
+            if (oauthConfig.GoogleOAuth != null)
+            {
+                manager.RegisterClient("Google", new TOAuth2Client(oauthConfig.GoogleOAuth, accessor));
+            }
+            if (oauthConfig.FacebookOAuth != null)
+            {
+                manager.RegisterClient("Facebook", new TOAuth2Client(oauthConfig.FacebookOAuth, accessor));
+            }
+            if (oauthConfig.LineOAuth != null)
+            {
+                manager.RegisterClient("Line", new TOAuth2Client(oauthConfig.LineOAuth, accessor));
+            }
+            if (oauthConfig.AzureOAuth != null)
+            {
+                manager.RegisterClient("Azure", new TOAuth2Client(oauthConfig.AzureOAuth, accessor));
+            }
+
+            return manager;
+        }
+    }
+
+}
--- samples/OAuthAspNetCore/Program.cs
+++ samples/OAuthAspNetCore/Program.cs
@@ -1,5 +1,5 @@
 using Bee.OAuth2.AspNetCore;
-using OAuthAspNetCore.Models;
+using OAuthAspNetCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,39 +9,10 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 // 讀取 OAuth2 設定
-builder.Configuration.AddJsonFile("OAuthConfig.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile(@"OAuthConfig.json", optional: false, reloadOnChange: true);
 
-// 建立 TOAuth2Manager 並註冊各 Provider
-builder.Services.AddSingleton<TOAuth2Manager>(provider =>
-{
-    var config = provider.GetRequiredService<IConfiguration>();
-    var accessor = provider.GetRequiredService<IHttpContextAccessor>();
-
-    var oauthConfig = config.Get<OAuthConfig>();
-    var manager = new TOAuth2Manager(accessor);
-
-    if (oauthConfig == null)
-    {
-        throw new InvalidOperationException("OAuth configuration is missing.");
-    }
-    if (oauthConfig.GoogleOAuth != null)
-    {
-        manager.RegisterClient("Google", new TOAuth2Client(oauthConfig.GoogleOAuth, accessor));
-    }
-    if (oauthConfig.FacebookOAuth != null)
-    {
-        manager.RegisterClient("Facebook", new TOAuth2Client(oauthConfig.FacebookOAuth, accessor));
-    }
-    if (oauthConfig.LineOAuth != null)
-    {
-        manager.RegisterClient("Line", new TOAuth2Client(oauthConfig.LineOAuth, accessor));
-    }
-    if (oauthConfig.AzureOAuth != null)
-    {
-        manager.RegisterClient("Azure", new TOAuth2Client(oauthConfig.AzureOAuth, accessor));
-    }
-    return manager;
-});
+// Register TOAuth2Manager as a singleton service using the custom factory method: OAuth2RegistrationHelper.CreateOAuth2Manager
+builder.Services.AddSingleton<TOAuth2Manager>(OAuth2RegistrationHelper.CreateOAuth2Manager);
 
 var app = builder.Build();
 
